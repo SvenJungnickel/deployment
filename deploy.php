4a3a906e841959fb1d0e586f65ab5144eb83d3da
Sven Jungnickel <svenjungnickel@googlemail.com>
2017-04-12 18:32:21 +1000

    update deployment script

--- deploy.php
+++ deploy.php
@@ -2,46 +2,48 @@
 /**
  * GIT DEPLOYMENT SCRIPT
  *
- * Used for automatically deploying websites via github or bitbucket, more deets here:
+ * Used for automatically deploying websites via github securely, more deets here:
  *
- *		https://gist.github.com/1809044
+ *      https://gist.github.com/limzykenneth/baef1b190c68970d50e1
  */
+
+// The header information which will be verified
+$agent=$_SERVER['HTTP_USER_AGENT'];
+$signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
+$body=@file_get_contents('php://input');
+
 // The commands
 $commands = array(
-    'echo $PWD',
-    'whoami',
-    'git pull',
-    'git status',
+    'git pull origin master',
     'git submodule sync',
-    'git submodule update',
-    'git submodule status',
+    'git submodule update'
 );
-// Run the commands for output
-$output = '';
-foreach($commands AS $command){
-    // Run it
-    $tmp = shell_exec($command);
-    // Output
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
-}
-// Make it pretty for manual user access (and why not?)
-?>
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
 
-    <?php echo $output; ?>
-</pre>
-</body>
-</html>
+base64_encode($agent);
+base64_encode($signature);
+
+if (strpos($agent,'GitHub-Hookshot') !== false){
+    if (hash_equals($signature, verify_request())){
+        // Run the commands
+        foreach($commands AS $command){
+            // Run it
+            $tmp = shell_exec($command);
+        }
+        echo "Deploy successful.";
+    }else{
+        header('HTTP/1.1 403 Forbidden');
+        echo "Invalid request 1.";
+    }
+}else{
+    header('HTTP/1.1 403 Forbidden');
+    echo "Invalid request 2.";
+}
+
+// Generate the hash verification with the request body and the key stored in your .htaccess file
+function verify_request(){
+    $message = $GLOBALS['body'];
+    $key     = $_ENV['GIT_TOKEN'];
+    $hash    = hash_hmac("sha1", $message, $key);
+    $hash = "sha1=".$hash;
+    return $hash;
+}
